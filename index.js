e8dc7fb3723f838be862a27453c9132f5e6d2c1d
kimseeun10 <tpdms981@gmail.com>
2024-03-05 00:55:22 +0900

    postman연결 성공

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const config = require('./config/key');
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 
-const config = require('./config/key');
+// application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
@@ -13,4 +20,22 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요'));
 
+app.post('/', async (req, res) => {
+
+    //회원가입시 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+    await user
+    .save()
+    .then(() => {
+        res.status(200).json({
+            success: true,
+        });
+    })
+    .catch((err) => {
+        console.error(err);
+        res.json({
+            success: false,
+            err: err,
+        });
+    });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
