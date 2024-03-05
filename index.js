86b6de0da1cc21fd6779a599b316f4a29b902646
kimseeun10 <tpdms981@gmail.com>
2024-03-05 16:49:57 +0900

    로그인

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const { User } = require("./models/User");
 
 // application/x-www-from-urlencoded
@@ -11,6 +12,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 // application/json
 app.use(bodyParser.json());
 
+// cookieParser
+app.use(cookieParser());
+
 const mongoose = require('mongoose');
 mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
@@ -18,24 +22,57 @@ mongoose.connect(config.mongoURI, {
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요'));
+app.get('/', (req, res) => res.send('정말 취업하고싶어요. 일하고싶어요.'));
 
 app.post('/', async (req, res) => {
 
-    //회원가입시 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
-    await user
-    .save()
-    .then(() => {
+    // 회원가입시 필요한 정보들을 client에서 가져오면 데이터베이스에 넣어준다.
+    const user = new User(req.body);
+
+    try {
+        await user.save();
         res.status(200).json({
             success: true,
         });
-    })
-    .catch((err) => {
+    } catch (err) {
         console.error(err);
-        res.json({
+        res.status(500).json({
             success: false,
-            err: err,
+            error: err.message,
         });
-    });
+    }
 });
+
+app.post('/login', async (req, res) => {
+    try {
+        // 1. 요청된 이메일을 데이터베이스에 있는지 찾는다.
+        const user = await User.findOne({ email: req.body.email }).exec();
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다."
+            });
+        }
+
+        // 2. 요청된 이메일이 db에 있다면 비밀번호가 일치하는지 확인.
+        const isMatch = await user.comparePassword(req.body.password);
+        if (!isMatch) {
+            return res.json({ loginSuccess: false, message: "비밀번호를 확인해주세요." });
+        }
+
+        // 3. 비밀번호까지 일치한다면 token 을 생성하기.
+        await user.generateToken();
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지...
+        res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            success: false,
+            error: err.message,
+        });
+    }
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`)); 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -7,8 +10,8 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        trim: true, // 스페이스 없애주는 역할
-        unique: 1 // 똑같은 값 사용하지 못하게, 중복 방지
+        trim: true,
+        unique: 1
     },
     password: {
         type: String,
@@ -29,8 +32,44 @@ const userSchema = mongoose.Schema({
     tokenExp: {
         type: Number
     }
-})
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre('save', function(next) {
+    if (this.isModified('password')) {
+        bcrypt.genSalt(saltRounds, (err, salt) => {
+            if (err) return next(err);
+            bcrypt.hash(this.password, salt, (err, hash) => {
+                if (err) return next(err);
+                this.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
 
-module.exports = {User} //다른곳에서도 사용할 수 있게
+userSchema.methods.comparePassword = function(plainPassword) {
+    return new Promise((resolve, reject) => {
+        bcrypt.compare(plainPassword, this.password, (err, isMatch) => {
+            if (err) return reject(err);
+            resolve(isMatch);
+        });
+    });
+};
+
+userSchema.methods.generateToken = async function() {
+    var user = this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+    user.token = token;
+    try {
+        await user.save();
+        return user;
+    } catch (err) {
+        throw err;
+    }
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = { User };
